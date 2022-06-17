df89e2fe5c365230b5d3e883808aed4f8ab280b8
David Ackerman <david@dvacreative.com>
2022-06-16 22:13:36 -0400

    progress

--- src/ThemeSwitcher.js
+++ src/ThemeSwitcher.js
@@ -48,13 +48,18 @@ export default function ThemeSwitcher(props) {
       </select>
       <div className={`fontModeButton sansButton ${sansActive}`} onClick={()=>{setSans()}}>Ag<span className="fontButtonLabel">SANS</span></div>
       <div className={`fontModeButton serifButton ${serifActive}`} onClick={()=>{setSerif()}}>Ag<span className="fontButtonLabel">SERIF</span></div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h1>header</h1>
+      <h5>subheader</h5>
+      <h5 className="highlight">highlight</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <div className="bgWrap">
       <div className="colorSwatch bg1"></div>
       <div className="colorSwatch bg2"></div>
       <div className="colorSwatch bg3"></div>
       <div className="colorSwatch bg4"></div>
       <div className="colorSwatch bg5"></div>
       <div className="colorSwatch bg6"></div>
+      </div>
       </div>
    </div>
   );
